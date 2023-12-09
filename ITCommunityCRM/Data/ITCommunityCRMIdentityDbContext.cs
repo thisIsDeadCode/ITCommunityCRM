ff8f8e928984f74318eeb24c994db6e022e6f291
thisIsDeadCode <105701529+thisIsDeadCode@users.noreply.github.com>
2023-12-09 19:52:59 +0300

    Add project files.

---
+++ ITCommunityCRM/Data/ITCommunityCRMIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITCommunityCRM.Data
+{
+    public class ITCommunityCRMIdentityDbContext : IdentityDbContext
+    {
+        public ITCommunityCRMIdentityDbContext(DbContextOptions<ITCommunityCRMIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
