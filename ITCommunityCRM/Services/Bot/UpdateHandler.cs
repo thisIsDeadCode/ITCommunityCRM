73f7b50d92c0262b1ef8f7094b19d6cc2561c4d3
igubanov <ivan@gubanov.cf>
2023-12-16 00:22:43 +0200

    handle start commands, send user response

--- ITCommunityCRM/Services/Bot/UpdateHandler.cs
+++ ITCommunityCRM/Services/Bot/UpdateHandler.cs
@@ -1,5 +1,8 @@
 ﻿using ITCommunityCRM.Data;
 using ITCommunityCRM.Data.Models;
+using MailKit;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -11,46 +14,88 @@ namespace ITCommunityCRM.Services.Bot
 {
     public class UpdateHandler : IUpdateHandler
     {
-        private readonly ITelegramBotClient _botClient;
+        private const string HELLO_MESSAGE = "Привет, дорогой друг. Добро пожаловать в наше сообщество.";
+        private const string WRONG_EVENT_ID = "Информация о событии не найдена";
         private readonly ILogger<UpdateHandler> _logger;
         private readonly ITCommunityCRMDbContext _context;
 
-        public UpdateHandler(ITelegramBotClient botClient, ILogger<UpdateHandler> logger, IServiceProvider serviceProvider,
-            ITCommunityCRMDbContext context)
+        public UpdateHandler(ILogger<UpdateHandler> logger, ITCommunityCRMDbContext context)
         {
-            _botClient = botClient;
             _logger = logger;
             _context = context;
         }
 
-        public async Task HandleUpdateAsync(ITelegramBotClient _, Update update, CancellationToken cancellationToken)
+        public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
         {
             var handler = update switch
             {
-                { Message: { } message } => BotOnMessageReceived(message, cancellationToken),
+                { Message: { } message } => BotOnMessageReceived(bot, message, cancellationToken),
                 _ => UnknownUpdateHandlerAsync(update, cancellationToken)
             };
 
             await handler;
         }
 
-        private async Task BotOnMessageReceived(Message message, CancellationToken cancellationToken)
+        private async Task BotOnMessageReceived(ITelegramBotClient bot, Message message, CancellationToken cancellationToken)
         {
-            if (message.Type == MessageType.Text && message.Text == "/Start")
+            if (message.Type != MessageType.Text
+                || message.Text?.StartsWith("/start", StringComparison.OrdinalIgnoreCase) != true)
             {
-                var group = _context.Groups.FirstOrDefault(x => x.GroupIdCode == message.Chat.Id);
-                if(group == null)
-                {
-                    _context.Groups.Add(new Group()
-                    {
-                        GroupIdCode = message.Chat.Id,
-                        GroupName = message.Chat.Username ?? message.Chat.Title,
-                        IsActive = true,
-                    });
-                }
+                return;
             }
 
-            return;
+            await TryCreateGroupAsync(message, cancellationToken);
+            await SendResponseToStartCommand(bot, message, cancellationToken);
+        }
+
+        private async Task SendResponseToStartCommand(ITelegramBotClient bot, Message message, CancellationToken cancellationToken)
+        {
+            var chatId = message.Chat.Id;
+            var messageParts = message.Text?.Split(" ").ToArray() ?? Array.Empty<string>();
+
+            if (messageParts.Length != 2)
+            {
+                _logger.LogInformation("Start command doesnt contains eventId parameters, send hello message.");
+                await bot.SendTextMessageAsync(chatId, HELLO_MESSAGE, cancellationToken: cancellationToken);
+                return;
+            }
+
+
+            var eventInfo = int.TryParse(messageParts[1], out var eventIdFromMessage)
+                ? await _context.Events.FirstOrDefaultAsync(x => x.Id == eventIdFromMessage, cancellationToken)
+                : null;
+
+            if (eventInfo == null)
+            {
+                _logger.LogInformation("Empty eventId in start command.");
+                await bot.SendTextMessageAsync(chatId, WRONG_EVENT_ID, cancellationToken: cancellationToken);
+                return;
+            }
+
+            _logger.LogInformation("Find event, sending information to user");
+            await bot.SendTextMessageAsync(chatId, GetMessageFromEvent(eventInfo), cancellationToken: cancellationToken);
+
+        }
+
+        private string GetMessageFromEvent(Event @event)
+        {
+            return $"{@event.Name}\r\n{@event.Description}";
+        }
+
+        // ?? it can be private a chat an user with the bot
+        private async Task TryCreateGroupAsync(Message message, CancellationToken cancellationToken)
+        {
+            var isGroupAlreadyCreated = await _context.Groups.AnyAsync(x => x.GroupIdCode == message.Chat.Id, cancellationToken);
+            if (!isGroupAlreadyCreated)
+            {
+                _context.Groups.Add(new Group()
+                {
+                    GroupIdCode = message.Chat.Id,
+                    GroupName = message.Chat.Username ?? message.Chat.Title ?? string.Empty,
+                    IsActive = true,
+                });
+                await _context.SaveChangesAsync(cancellationToken);
+            }
         }
 
         private Task UnknownUpdateHandlerAsync(Update update, CancellationToken cancellationToken)
