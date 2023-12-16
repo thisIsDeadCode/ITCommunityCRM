ff1251761295901441eac30d988cef0d2eddd3c4
igubanov <ivan@gubanov.cf>
2023-12-16 17:10:54 +0200

    base redirect without update db

---
+++ ITCommunityCRM/Controllers/RedirectController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ITCommunityCRM.Data;
+using ITCommunityCRM.Services;
+
+namespace ITCommunityCRM.Controllers
+{
+    public class RedirectController : ControllerBase
+    {
+        private readonly RedirectLinkService linkService;
+
+        public RedirectController(RedirectLinkService linkService)
+        {
+            this.linkService = linkService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var queries = HttpContext.Request.QueryString.ToString();
+            if (string.IsNullOrEmpty(queries))
+            {
+                return NotFound();
+            }
+
+            var redirectTo = linkService.GetOriginalUrl(queries);
+            linkService.TrackRedirect(queries);
+
+            return Redirect(redirectTo);
+        }
+
+    }
+}
